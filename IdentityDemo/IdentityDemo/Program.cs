f884b4052d61b493487839ea62e507e7f99a3499
Satish <satish32@gmail.com>
2017-05-02 06:59:53 -0500

    Added nuget packages

--- IdentityDemo/IdentityDemo/Program.cs
+++ IdentityDemo/IdentityDemo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace IdentityDemo
 {
@@ -10,6 +12,14 @@ namespace IdentityDemo
     {
         static void Main(string[] args)
         {
+            var userStroe = new UserStore<IdentityUser>();
+            var userMgr = new UserManager<IdentityUser>(userStroe);
+
+            var result = userMgr.Create(new IdentityUser("satish32@gmail.com"),"password");
+
+            Console.WriteLine("User Created: {0}", result.Succeeded);
+
+            Console.ReadLine();
         }
     }
 }
